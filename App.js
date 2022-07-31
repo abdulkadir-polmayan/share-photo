3033317b8eea67a5fe96c24f35af96216345e4d7
abdulkadir-polmayan <abdulkadirsenel63@gmail.com>
2022-07-31 08:53:40 +0300

    learn expo

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {Image, StyleSheet, Text,TouchableOpacity, View,  } from 'react-native';
+import logo from './assets/logo.png'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image source={logo} style={styles.logo} />
+      <Text style={styles.instruction} >
+        click the button to share
+      </Text>
+      <TouchableOpacity
+        onPress={()=> alert("hello world")}
+        >
+        <Text>frf</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
+
     </View>
   );
 }
@@ -17,4 +27,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  logo:{
+    width:305 ,
+    height:159,
+    marginBottom:50
+  },
+  instruction:{
+    color:"#888", 
+    fontSize:18,
+    marginHorizontal:50
+  }
+  
 });
